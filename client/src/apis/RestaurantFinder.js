8f0aa429cab19ad46e3831575c42caaf5d0c720c
Henry Vu <henryvu25@gmail.com>
2020-12-14 17:38:50 -0800

    added slash to server endpoint

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const baseURL = process.env.NODE_ENV === "production" ? "api/v1/restaurants" : "http://localhost:3005/api/v1/restaurants"
+const baseURL = process.env.NODE_ENV === "production" ? "/api/v1/restaurants" : "http://localhost:3005/api/v1/restaurants"
 //allows you to import this baseURL from other components
 export default axios.create({
     baseURL: baseURL,
